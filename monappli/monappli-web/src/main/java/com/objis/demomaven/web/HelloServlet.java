ee71a756771b159549d97c99f8b11356f31bcad8
Shayma <shaima.nasri@gmail.com>
2014-10-16 22:26:15 +0100

    commit init

---
+++ monappli/monappli-web/src/main/java/com/objis/demomaven/web/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.objis.demomaven.web;
+
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+ protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+ throws ServletException, IOException {
+   response.setContentType("text/html");
+   PrintWriter out = response.getWriter();
+   out.println("Projet Maven la centrale-web : Servlet Lanc�e");
+   out.flush();
+   out.close();
+ }                                  
+}
